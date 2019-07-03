c942fcccad44c46734e2719ea418dae009802861
RJ <rongjihuang@gmail.com>
2019-07-03 14:07:04 +0800

    Use JUnit5 instead of JUnit4

--- src/test/java/tech/simter/template/impl/FreeMarkerEngineTest.java
+++ src/test/java/tech/simter/template/impl/FreeMarkerEngineTest.java
@@ -1,9 +1,7 @@
 package tech.simter.template.impl;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import tech.simter.template.TemplateEngine;
 import tech.simter.template.bean.User;
 
@@ -12,22 +10,19 @@ import javax.inject.Named;
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * @author RJ
  */
-@RunWith(SpringRunner.class)
-//@ContextConfiguration(classes = {FreeMarkerEngine.class, FreeMarkerConfiguration.class})
-@ContextConfiguration(classes = {FreeMarkerEngine.class})
-public class FreeMarkerEngineTest {
+@SpringJUnitConfig(FreeMarkerEngine.class)
+class FreeMarkerEngineTest {
   @Inject
   @Named(TemplateEngine.TYPE_FREEMARKER)
   private TemplateEngine templateEngine;
 
   @Test
-  public void renderTemplate() {
+  void renderTemplate() {
     // Create a data-model
     Map<String, Object> data = new HashMap<>();
     data.put("msg", "Hello");
@@ -35,26 +30,26 @@ public class FreeMarkerEngineTest {
     // default root resource path is '/'
 
     // load resource 'src/test/resources/test.ftl' and render it
-    assertThat(templateEngine.renderTemplate("test.ftl", data), is("/test.ftl:Hello"));
+    assertEquals("/test.ftl:Hello", templateEngine.renderTemplate("test.ftl", data));
 
     // load resource 'src/test/resources/templates/test.ftl' and render it
-    assertThat(templateEngine.renderTemplate("templates/test.ftl", data), is("/templates/test.ftl:Hello"));
+    assertEquals("/templates/test.ftl:Hello", templateEngine.renderTemplate("templates/test.ftl", data));
   }
 
   @Test
-  public void renderContent() {
+  void renderContent() {
     // Create a data-model
     Map<String, Object> data = new HashMap<>();
     data.put("msg", "Hello");
 
     // Render
-    assertThat(templateEngine.renderContent("--${msg}--", data), is("--Hello--"));
+    assertEquals("--Hello--", templateEngine.renderContent("--${msg}--", data));
 
     // Render
     User user = new User();
     user.id = 1; // freeMarker not support field
     user.setAccount("test");
     data.put("user", user);
-    assertThat(templateEngine.renderContent("${user.id!2}/${user.account!'a'}", data), is("2/test"));
+    assertEquals("2/test", templateEngine.renderContent("${user.id!2}/${user.account!'a'}", data));
   }
 }
